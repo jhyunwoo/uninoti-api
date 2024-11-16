15e84139a2c09a3b18f166ac687ad329f0493003
jhyunwoo <jhyunwoo0228@gmail.com>
2024-11-16 19:07:26 +0900

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import admissionChecker from "./admission-checker";
 import { csrf } from "hono/csrf";
 import { cors } from "hono/cors";
 
-const sites = ["https://unoti.moveto.kr"];
+const sites = ["https://uninoti.moveto.kr"];
 
 const app = new Hono();
 
