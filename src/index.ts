7401dc928fde3d911a2363788479ec361bbcf882
jhyunwoo <jhyunwoo0228@gmail.com>
2024-12-13 13:53:09 +0900

    finish service

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import { Hono } from "hono";
-import admissionChecker from "./admission-checker";
 import { csrf } from "hono/csrf";
 import { cors } from "hono/cors";
-import countInText from "./count-in-text";
 
 const sites = ["https://uninoti.moveto.kr", "http://localhost:3000"];
 
@@ -36,15 +34,15 @@ app.get("/snu-final", async (c) => {
  * 연세대 수시 최종 합격자 발표
  */
 app.get("/yonsei-final", async (c) => {
-  const url =
-    "https://admission.yonsei.ac.kr/seoul/admission/html/rolling/notice.asp";
+  // const url =
+  //   "https://admission.yonsei.ac.kr/seoul/admission/html/rolling/notice.asp";
 
-  return c.json({
-    state: (await countInText(url, "합격자", "euc-kr")) > 16,
-  });
   // return c.json({
-  //   state: true,
+  //   state: (await countInText(url, "합격자", "euc-kr")) > 16,
   // });
+  return c.json({
+    state: true,
+  });
 });
 
 /**
@@ -80,12 +78,12 @@ app.get("/hanyang-final", async (c) => {
 });
 
 app.get("/snu-jigyun-1st", async (c) => {
-  const url = "https://admission.snu.ac.kr/undergraduate/notice";
-
-  const state = await admissionChecker(url, ["2025", "지역균형전형", "합격자"]);
+  // const url = "https://admission.snu.ac.kr/undergraduate/notice";
+  //
+  // const state = await admissionChecker(url, ["2025", "지역균형전형", "합격자"]);
 
   return c.json({
-    state: state,
+    state: true,
   });
 });
 
@@ -96,34 +94,40 @@ app.get("/yonsei-international-1st", (c) => {
 });
 
 app.get("/yonsei-talent-international-final", async (c) => {
-  const url =
-    "https://admission.yonsei.ac.kr/seoul/admission/html/rolling/notice.asp";
-
-  const state = await admissionChecker(
-    url,
-    ["2025학년도", "수시모집", "특기자", "국제인재", "최종"],
-    "euc-kr",
-  );
+  // const url =
+  //   "https://admission.yonsei.ac.kr/seoul/admission/html/rolling/notice.asp";
+  //
+  // const state = await admissionChecker(
+  //   url,
+  //   ["2025학년도", "수시모집", "특기자", "국제인재", "최종"],
+  //   "euc-kr",
+  // );
 
   return c.json({
-    state: state,
+    state: true,
   });
 });
 
 app.get("/korea-gaejuck-final", async (c) => {
-  const url = "https://oku.korea.ac.kr/oku/index.do";
-
+  // const url = "https://oku.korea.ac.kr/oku/index.do";
+  //
+  // return c.json({
+  //   state: (await countInText(url, "계열적합전형")) > 9,
+  // });
   return c.json({
-    state: (await countInText(url, "계열적합전형")) > 9,
+    state: true,
   });
 });
 
 app.get("/cau-tamgu-1st", async (c) => {
-  const url =
-    "http://admission.cau.ac.kr/submenu.do?menuurl=n5%2fP1yX8Zyh%2fvtvla1KeyA%3d%3d&";
-  const state = await admissionChecker(url, ["25학년도", "탐구형", "합격자"]);
+  // const url =
+  //   "http://admission.cau.ac.kr/submenu.do?menuurl=n5%2fP1yX8Zyh%2fvtvla1KeyA%3d%3d&";
+  // const state = await admissionChecker(url, ["25학년도", "탐구형", "합격자"]);
+  // return c.json({
+  //   state: state,
+  // });
   return c.json({
-    state: state,
+    state: true,
   });
 });
 
